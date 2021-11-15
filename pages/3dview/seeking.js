b9cbe1a8647074d32917d18beed085579d68794d
Matthew Wang <wmatthew123@gmail.com>
2021-11-15 17:24:26 +0800

    3d

---
+++ pages/3dview/seeking.js
@@ -0,0 +1,21 @@
+import Layout from '../../layout';
+
+const Contacts = () => (
+    < Layout >
+        <main className="three-d-view" id="3d-seeking" dangerouslySetInnerHTML={{
+            __html:
+                `<div style="max-width: 800px; max-height: 501px;">
+                    <div style="left: 0px; width: 100%; height: 0px; position: relative; padding-bottom: 62.625%; overflow: hidden;">
+                        <iframe src="/animations/seeking_XR.5/seeking_XR.5.html"
+                                    allowfullscreen
+                                    style="position: absolute; top: 0px; left: 0px; height: 100%; width: 1px; min-width: 100%; *width: 100%;"
+                                    frameborder="0"
+                                    scrolling="no">
+                        </iframe>
+                    </div>
+                </div>`
+        }}></main>
+    </Layout >
+)
+
+export default Contacts;
