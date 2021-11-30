4d7b8ac52f3388008d2081369f9d1178886afaaa
Matthew Wang <wmatthew123@gmail.com>
2021-12-01 00:34:55 +0800

    mobile ver

--- pages/3dview/seeking-wdgt.js
+++ pages/3dview/seeking-wdgt.js
@@ -4,11 +4,11 @@ const Contacts = () => (
     < Layout >
         <main className="three-d-view" id="3d-seeking" dangerouslySetInnerHTML={{
             __html:
-                `<div style="max-width: 100%; max-height: 100vh;">
-                    <div style="left: 0px; width: 100%; height: 0px; position: relative; padding-bottom: 62.625%; overflow: hidden;">
+                `<div style="max-width: 100%; height: 100vh;">
+                    <div style="left: 0px; width: 100%; height: 100vh; position: relative; padding-bottom: 62.625%; overflow: hidden;">
                         <iframe src="/animations/seeking_XR.4.wdgt/seeking_XR.4.html"
                                     allowfullscreen
-                                    style="position: absolute; top: 0px; left: 0px; height: 100%; width: 1px; min-width: 100%; *width: 100%;"
+                                    style="position: absolute; top: 0px; left: 0px; height: 100vh; width: 1px; min-width: 100%; *width: 100%;"
                                     frameborder="0"
                                     scrolling="no">
                         </iframe>
