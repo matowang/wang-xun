edc1cda73e3cd604d15cd59f2084eb611e436c87
Matthew Wang <wmatthew123@gmail.com>
2021-12-01 00:05:50 +0800

    added another one

---
+++ pages/3dview/seeking-wdgt.js
@@ -0,0 +1,21 @@
+import Layout from '../../layout';
+
+const Contacts = () => (
+    < Layout >
+        <main className="three-d-view" id="3d-seeking" dangerouslySetInnerHTML={{
+            __html:
+                `<div style="max-width: 100%; max-height: 100vh;">
+                    <div style="left: 0px; width: 100%; height: 0px; position: relative; padding-bottom: 62.625%; overflow: hidden;">
+                        <iframe src="/animations/seeking_XR.4.wdgt/seeking_XR.4.html"
+                                    allowfullscreen
+                                    style="position: absolute; top: 0px; left: 0px; height: 100%; width: 1px; min-width: 100%; *width: 100%;"
+                                    frameborder="0"
+                                    scrolling="no">
+                        </iframe>
+                    </div>
+                </div>`
+        }}></main>
+    </Layout >
+)
+
+export default Contacts;
