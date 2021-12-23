7d10bdcb591e78e8554f66df9c2f6fcda69f9bd4
Matthew Wang <wmatthew123@gmail.com>
2021-12-23 17:55:13 +0800

    updated seeking

--- pages/3dview/seeking-wdgt.js
+++ pages/3dview/seeking-wdgt.js
@@ -6,7 +6,7 @@ const Contacts = () => (
             __html:
                 `<div style="max-width: 100%; height: 100vh;">
                     <div style="left: 0px; width: 100%; height: 100vh; position: relative; padding-bottom: 62.625%; overflow: hidden;">
-                        <iframe src="/animations/seeking_XR.4.wdgt/seeking_XR.4.html"
+                        <iframe src="/animations/seeking-xr_1217_2021.163.wdgt/seeking-xr_1217_2021.163.html"
                                     allowfullscreen
                                     style="position: absolute; top: 0px; left: 0px; height: 100vh; width: 1px; min-width: 100%; *width: 100%;"
                                     frameborder="0"
